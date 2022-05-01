826eb8c2355e06127308806876f6f92b62d44c07
Yuhan Wu <398789568@qq.com>
2022-04-30 23:57:45 -0400

    checkbox for rounds

--- assets/Script/RoomChoice.js
+++ assets/Script/RoomChoice.js
@@ -4,11 +4,17 @@ cc.Class({
       properties: {
             joinGameWindow: cc.Node,
             createRoomWindow: cc.Node,
+            checkbox6Rounds: cc.Sprite,
+            checkbox12Rounds: cc.Sprite,
+
+            uncheckedSprite:cc.SpriteFrame,
+            checkedSprite:cc.SpriteFrame,
       },
 
       // use this for initialization
       onLoad: function () {
-          
+          this.selected6Rounds = false;
+          this.on6RoundsSelected();
       },
 
       onCreateRoomClicked: function() {
@@ -25,6 +31,26 @@ cc.Class({
 
       onCloseJoinRoomClicked: function() {
             this.joinGameWindow.active = false;
+      },
+
+      on6RoundsSelected: function() {
+            console.log("on6RoundsSelected");
+            if (this.selected6Rounds) {
+                  return;
+            }
+            this.selected6Rounds = true;
+            this.checkbox6Rounds.spriteFrame = this.checkedSprite;
+            this.checkbox12Rounds.spriteFrame = this.uncheckedSprite;
+      },
+      
+      on12RoundsSelected: function() {
+            console.log("on12RoundsSelected");
+            if (!this.selected6Rounds) {
+                  return;
+            }
+            this.selected6Rounds = false;
+            this.checkbox6Rounds.spriteFrame = this.uncheckedSprite;
+            this.checkbox12Rounds.spriteFrame = this.checkedSprite;
       },
 });
   
